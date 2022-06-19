07331b6afb70ec32cf57aac4c83064c8a261a3da
StarTux <ths.loy@googlemail.com>
2022-06-19 14:33:53 +0200

    Leading and Trailing newline

--- src/main/java/com/cavetale/sidebar/PlayerListFooter.java
+++ src/main/java/com/cavetale/sidebar/PlayerListFooter.java
@@ -2,10 +2,13 @@ package com.cavetale.sidebar;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
+import static net.kyori.adventure.text.Component.join;
+import static net.kyori.adventure.text.Component.newline;
+import static net.kyori.adventure.text.JoinConfiguration.noSeparators;
 
 public final class PlayerListFooter extends PlayerListDisplay {
     @Override
     protected void send(Player player, Component message) {
-        player.sendPlayerListFooter(message);
+        player.sendPlayerListFooter(join(noSeparators(), message, newline()));
     }
 }
