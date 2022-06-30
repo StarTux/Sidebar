89c273a597cd9de8cb4797d467687b30106eff12
StarTux <ths.loy@googlemail.com>
2022-07-01 01:36:40 +0200

    Time and location

--- src/main/java/com/cavetale/sidebar/Session.java
+++ src/main/java/com/cavetale/sidebar/Session.java
@@ -2,11 +2,18 @@ package com.cavetale.sidebar;
 
 import com.cavetale.core.event.hud.PlayerHudEntry;
 import com.cavetale.core.event.hud.PlayerHudEvent;
+import com.cavetale.core.event.hud.PlayerHudPriority;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import lombok.Getter;
+import org.bukkit.GameRule;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 @Getter
 public final class Session {
@@ -36,6 +43,8 @@ public final class Session {
         bossbar.disable(player);
     }
 
+    private static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm:ss");
+
     protected void tick(Player player) {
         PlayerHudEvent playerHudEvent = new PlayerHudEvent(player);
         playerHudEvent.callEvent();
@@ -48,8 +57,22 @@ public final class Session {
             entries.addAll(playerHudEvent.getSidebar());
             sidebar.loadEntries(entries);
         }
-        header.loadEntries(player, playerHudEvent.getHeader());
-        footer.loadEntries(player, playerHudEvent.getFooter());
+        List<PlayerHudEntry> headerList = new ArrayList<>();
+        headerList.addAll(playerHudEvent.getHeader());
+        headerList.add(new PlayerHudEntry(PlayerHudPriority.LOWEST.value,
+                                          List.of(text(TIME_FORMAT.format(new Date()), GRAY))));
+        header.loadEntries(player, headerList);
+        List<PlayerHudEntry> footerList = new ArrayList<>();
+        footerList.addAll(playerHudEvent.getFooter());
+        if (!player.getWorld().getGameRuleValue(GameRule.REDUCED_DEBUG_INFO)) {
+            Location loc = player.getLocation();
+            final int x = loc.getBlockX();
+            final int y = loc.getBlockY();
+            final int z = loc.getBlockZ();
+            footerList.add(new PlayerHudEntry(PlayerHudPriority.DEFAULT.value,
+                                              List.of(text(x + " " + y + " " + z, GRAY))));
+        }
+        footer.loadEntries(player, footerList);
         bossbar.loadEntries(player, playerHudEvent.getBossbar());
     }
 }
