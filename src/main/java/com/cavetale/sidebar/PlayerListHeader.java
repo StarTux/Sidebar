6b3522ed01a0eb2a35ad93f9b65f85414b38ce3d
StarTux <ths.loy@googlemail.com>
2023-02-17 17:45:56 +0100

    Skip the empty line

--- src/main/java/com/cavetale/sidebar/PlayerListHeader.java
+++ src/main/java/com/cavetale/sidebar/PlayerListHeader.java
@@ -2,13 +2,10 @@ package com.cavetale.sidebar;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
-import static net.kyori.adventure.text.Component.join;
-import static net.kyori.adventure.text.Component.newline;
-import static net.kyori.adventure.text.JoinConfiguration.noSeparators;
 
 public final class PlayerListHeader extends PlayerListDisplay {
     @Override
     protected void send(Player player, Component message) {
-        player.sendPlayerListHeader(join(noSeparators(), newline(), message));
+        player.sendPlayerListHeader(message);
     }
 }
